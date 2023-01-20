71831315316dc722f8c17245c1b83d925853e4ee
erben <erben@umich.edu>
2023-01-20 17:33:15 -0500

    initial

---
+++ with-tailwindcss-app/pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+    return(
+
+        <div class="flex">
+            <ul class="flex">
+                <li>Works</li>
+                <li>Information</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
